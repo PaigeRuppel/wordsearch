8b1f6fd5884d5616814b756fcff2aa9fb211ea43
paigeruppel <paige.ruppel@gmail.com>
2017-08-07 15:53:25 -0400

    passed second test for horizontal scan

--- src/main/java/wordsearch/HorizontalFinder.java
+++ src/main/java/wordsearch/HorizontalFinder.java
@@ -1,11 +1,17 @@
 package wordsearch;
 
 public class HorizontalFinder {
-// To satisfy the first user story
+	// To satisfy the first user story
 
 	public String horizontalScan(String toFind, char[][] toSearch) {
-		return "cat:(1,0)(1,1)(1,2)";
+		String answer = toFind+":";
+
+		for (int row = 0; row < toSearch.length; row++) {
+			if (toSearch[row][0] == toFind.charAt(0)) {
+				answer += "(" + row + ",0)(" + row + ",1)(" + row + ",2)";
+			}
+		}
+		return answer;
 	}
-	
-	
+
 }
--- src/test/java/wordsearch/HorizontalFinderTest.java
+++ src/test/java/wordsearch/HorizontalFinderTest.java
@@ -8,10 +8,19 @@ import org.junit.Test;
 public class HorizontalFinderTest {
 
 	@Test
-	public void shouldReturnCatAtOneZeroOneTwo() {
+	public void shouldReturnCatAtRowOneColumnZeroOneTwo() {
 		HorizontalFinder underTest = new HorizontalFinder();
 		String toFind = "cat";
 		char[][] toSearch = {{'x', 'x', 'x'},{'c','a', 't'},{'x','x','x'}};
 		assertThat(underTest.horizontalScan(toFind, toSearch), is("cat:(1,0)(1,1)(1,2)"));
+	}
+	
+	@Test
+	public void shouldReturnCatAtRowZeroColumnZeroOneTwo() {
+		HorizontalFinder underTest = new HorizontalFinder();
+		String toFind = "cat";
+		char[][] toSearch = {{'c','a', 't'},{'x', 'x', 'x'},{'x','x','x'}};
+		assertThat(underTest.horizontalScan(toFind, toSearch), is("cat:(0,0)(0,1)(0,2)"));
+		
 	}
 }
