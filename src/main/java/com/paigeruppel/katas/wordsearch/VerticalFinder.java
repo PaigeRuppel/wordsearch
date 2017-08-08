b06eff7a1b74367d0d584ec7c760acec5db10e48
paigeruppel <paige.ruppel@gmail.com>
2017-08-08 15:55:56 -0400

    wrote and passed another test for vertical finder

--- src/main/java/com/paigeruppel/katas/wordsearch/VerticalFinder.java
+++ src/main/java/com/paigeruppel/katas/wordsearch/VerticalFinder.java
@@ -2,11 +2,31 @@ package com.paigeruppel.katas.wordsearch;
 
 public class VerticalFinder {
 
+	private String toFind;
+	private LetterGrid grid;
+
+	private AnswerBuilder answer;
+
 	public VerticalFinder(String toFind, LetterGrid grid) {
+		this.toFind = toFind;
+		this.grid = grid;
+
+		answer = new AnswerBuilder(toFind);
 	}
 
 	public String verticalScan() {
-		return "cat: (1,0),(1,1),(1,2)";
+
+		for (int col = 0; col < grid.colLength; col++) {
+
+			if (grid.getCharacterAt(0, col) == toFind.charAt(0)) {
+				answer.buildAnswerMap(toFind.charAt(0), "(0,"+ col + ")");
+				answer.buildAnswerMap(toFind.charAt(1), "(1,"+ col + ")");
+				answer.buildAnswerMap(toFind.charAt(2), "(2,"+ col + ")");
+ 			}
+
+		}
+		return answer.generate();
+
 	}
 
 }
--- src/test/java/com/paigeruppel/katas/wordsearch/VerticalFinderTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/VerticalFinderTest.java
@@ -6,19 +6,27 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class VerticalFinderTest {
+
 	private LetterGrid grid;
 	private String toFind;
-
 
 	public VerticalFinder createTest(String toFind, LetterGrid grid) {
 		return new VerticalFinder(toFind, grid);
 	}
-	
+
 	@Test
-	public void shouldReturnCatAtRowOneColumnZeroOneTwo() {
+	public void shouldReturnCatAtColumnOneRowZeroOneTwo() {
 		toFind = "cat";
 		char[][] toSearch = { { 'x', 'c', 'x' }, { 'x', 'a', 'x' }, { 'x', 't', 'x' } };
 		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).verticalScan(), is("cat: (1,0),(1,1),(1,2)"));
+		assertThat(createTest(toFind, grid).verticalScan(), is("cat: (0,1),(1,1),(2,1)"));
+	}
+
+	@Test
+	public void shouldReturnCatAtColumnZeroRowZeroOneTwo() {
+		toFind = "cat";
+		char[][] toSearch = { { 'c', 'x', 'x' }, { 'a', 'x', 'x' }, { 't', 'x', 'x' } };
+		grid = new LetterGrid(toSearch);
+		assertThat(createTest(toFind, grid).verticalScan(), is("cat: (0,0),(1,0),(2,0)"));
 	}
 }
