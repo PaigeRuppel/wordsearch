ad589b6c3ef393dd71831ee2b3eaccf637443595
paigeruppel <paige.ruppel@gmail.com>
2017-08-08 11:32:41 -0400

    refactored tests

--- src/main/java/com/paigeruppel/katas/wordsearch/HorizontalFinder.java
+++ src/main/java/com/paigeruppel/katas/wordsearch/HorizontalFinder.java
@@ -1,9 +1,17 @@
 package com.paigeruppel.katas.wordsearch;
 
+// To satisfy the first user story
 public class HorizontalFinder {
-	// To satisfy the first user story
 
-	public String horizontalScan(String toFind, char[][] toSearch) {
+	private String toFind;
+	private char[][] toSearch;
+
+	public HorizontalFinder(String toFind, char[][] toSearch) {
+		this.toFind = toFind;
+		this.toSearch = toSearch;
+	}
+
+	public String horizontalScan() {
 		String answer = toFind + ":";
 
 		int ind = 0;
--- src/test/java/com/paigeruppel/katas/wordsearch/HorizontalFinderTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/HorizontalFinderTest.java
@@ -9,27 +9,27 @@ import com.paigeruppel.katas.wordsearch.HorizontalFinder;
 
 public class HorizontalFinderTest {
 
+	public HorizontalFinder createTest(String toFind, char[][]toSearch) {
+		return new HorizontalFinder(toFind, toSearch);
+	}
 	@Test
 	public void shouldReturnCatAtRowOneColumnZeroOneTwo() {
-		HorizontalFinder underTest = new HorizontalFinder();
 		String toFind = "cat";
 		char[][] toSearch = { { 'x', 'x', 'x' }, { 'c', 'a', 't' }, { 'x', 'x', 'x' } };
-		assertThat(underTest.horizontalScan(toFind, toSearch), is("cat:(1,0)(1,1)(1,2)"));
+		assertThat(createTest(toFind, toSearch).horizontalScan(), is("cat:(1,0)(1,1)(1,2)"));
 	}
 
 	@Test
 	public void shouldReturnCatAtRowZeroColumnZeroOneTwo() {
-		HorizontalFinder underTest = new HorizontalFinder();
 		String toFind = "cat";
 		char[][] toSearch = { { 'c', 'a', 't' }, { 'x', 'x', 'x' }, { 'x', 'x', 'x' } };
-		assertThat(underTest.horizontalScan(toFind, toSearch), is("cat:(0,0)(0,1)(0,2)"));
+		assertThat(createTest(toFind, toSearch).horizontalScan(), is("cat:(0,0)(0,1)(0,2)"));
 	}
 
 	@Test
 	public void shouldReturnLaAtRowZeroColumnOneTwo() {
-		HorizontalFinder underTest = new HorizontalFinder();
 		String toFind = "la";
 		char[][] toSearch = { { 'x', 'l', 'a' }, { 'x', 'x', 'x' }, { 'x', 'x', 'x' } };
-		assertThat(underTest.horizontalScan(toFind, toSearch), is("la:(0,1)(0,2)"));
+		assertThat(createTest(toFind, toSearch).horizontalScan(), is("la:(0,1)(0,2)"));
 	}
 }
