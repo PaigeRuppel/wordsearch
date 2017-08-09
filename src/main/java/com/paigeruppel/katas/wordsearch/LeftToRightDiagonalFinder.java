f4650041e1ec70028d4f146b4a28614a4e930db1
paigeruppel <paige.ruppel@gmail.com>
2017-08-09 17:07:11 -0400

    some refactoring in LToR diagonal finder

--- src/main/java/com/paigeruppel/katas/wordsearch/LeftToRightDiagonalFinder.java
+++ src/main/java/com/paigeruppel/katas/wordsearch/LeftToRightDiagonalFinder.java
@@ -10,69 +10,66 @@ public class LeftToRightDiagonalFinder {
 		this.grid = grid;
 	}
 
+	private int ind;
+	private int tries;
+	private int start;
+
 	public String scanAlongColumns() {
-		int ind = 0;
-		int tries = 0;
-		int start = 0;
+		ind = 0;
+		tries = 0;
+		start = 0;
 		while (tries < 2) {
-			for (int row = 0, col = start; row < grid.rowLength && col < grid.colLength; row++) {
+			for (int row = 0, col = start; row < grid.rowLength && col < grid.colLength; row++, col++) {
 				if (grid.getCharacterAt(row, col) == answer.currentChar(ind)) {
 					answer.buildAnswerMap(answer.currentChar(ind), answer.coords(row, col));
 					if (answer.maxInd(ind)) {
 						return answer.generate();
 					} else {
 						ind++;
-						if (grid.getNextCharacterLToRDiagonalFrom(row, col) != answer.currentChar(ind)) {
-							answer.reset();
-							ind = 0;
-						} else {
-							col++;
-						}
+						resetIfNextCharNotPresent(row, col);
 					}
-
 				}
 			}
-			if (start == grid.colLength - 1) {
-				start = 0;
-				answer.reverseWord();
-				tries++;
-			} else {
-				start++;
-			}
+			checkPositionAndContinueOrReverseWord();
 		}
 		return "not found";
 	}
 
 	public String scanAlongRows() {
-		int start = 1; // since row zero is scanned in alongColumn method
-		int ind = 0;
-		int tries = 0;
+		start = 1; // since row zero is scanned in alongColumn method
+		ind = 0;
+		tries = 0;
 		while (tries < 2) {
-			for (int row = start, col = 0; row < grid.rowLength && col < grid.colLength; col++) {
+			for (int row = start, col = 0; row < grid.rowLength && col < grid.colLength; row++, col++) {
 				if (grid.getCharacterAt(row, col) == answer.currentChar(ind)) {
 					answer.buildAnswerMap(answer.currentChar(ind), answer.coords(row, col));
 					if (answer.maxInd(ind)) {
 						return answer.generate();
 					} else {
 						ind++;
-						if (grid.getNextCharacterLToRDiagonalFrom(row, col) != answer.currentChar(ind)) {
-							answer.reset();
-							ind = 0;
-						} else {
-							row++;
-						}
+						resetIfNextCharNotPresent(row, col);
 					}
 				}
 			}
-			if (start == grid.rowLength - 1) {
-				start = 0;
-				answer.reverseWord();
-				tries++;
-			} else {
-				start++;
-			}
+			checkPositionAndContinueOrReverseWord();
 		}
 		return "";
 	}
 
+	private void checkPositionAndContinueOrReverseWord() {
+		if (start == grid.colLength - 1) {
+			start = 0;
+			answer.reverseWord();
+			tries++;
+		} else {
+			start++;
+		}
+	}
+
+	private void resetIfNextCharNotPresent(int row, int col) {
+		if (grid.getNextCharacterLToRDiagonalFrom(row, col) != answer.currentChar(ind)) {
+			answer.reset();
+			ind = 0;
+		}
+	}
 }
