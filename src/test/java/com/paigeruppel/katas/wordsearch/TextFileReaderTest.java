3ba0d66cfb7e0d2dca21637fb80dbc7f3309d895
paigeruppel <paige.ruppel@gmail.com>
2017-08-15 17:32:58 -0400

    wrote and passed first test to build letter grid in text file reader

--- src/test/java/com/paigeruppel/katas/wordsearch/TextFileReaderTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/TextFileReaderTest.java
@@ -5,15 +5,23 @@ import static org.junit.Assert.assertThat;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextFileReaderTest {
 
+	private String starTrekFile = "C:\\Users\\Paige\\code\\code-katas\\wordsearch\\src\\main\\resources\\StarTrekSearch.txt";
+	private String oakSpeciesFile = "C:\\Users\\Paige\\code\\code-katas\\wordsearch\\src\\main\\resources\\OakSpeciesSearch.txt";
+	
+	private TextFileReader underTest;
+	
+	@Before
+	public void setup() {
+		underTest = new TextFileReader();
+	}
 	
 	@Test
 	public void shouldReadFirstLineStarTrekSearchAsAStringArrayList() {
-		String starTrekFile = "C:\\Users\\Paige\\code\\code-katas\\wordsearch\\src\\main\\resources\\StarTrekSearch.txt";
-		TextFileReader underTest = new TextFileReader();
 		ArrayList<String> listToFind = new ArrayList<>();
 		listToFind.add(0, "BONES");
 		listToFind.add(1, "KHAN");
@@ -27,8 +35,6 @@ public class TextFileReaderTest {
 	
 	@Test
 	public void shouldReadFirstLineOakSpeciesSearchAsAStringArrayList() {
-		String oakSpeciesFile = "C:\\Users\\Paige\\code\\code-katas\\wordsearch\\src\\main\\resources\\OakSpeciesSearch.txt";
-		TextFileReader underTest = new TextFileReader();
 		ArrayList<String> listToFind = new ArrayList<>();
 		listToFind.add(0, "ACERIFOLIA");
 		listToFind.add(1, "ALBUS");
@@ -42,6 +48,28 @@ public class TextFileReaderTest {
 		listToFind.add(9, "VELUTINA");
 		assertThat(underTest.readFirstLine(oakSpeciesFile), is(listToFind));
 	}
+	
+	@Test
+	public void shouldReadLetterGridAsCharArrayArrayFromStarTrekSearch() {
+		char[][] starTrekGrid = {
+				{'U','M','K','H','U','L','K','I','N','V','J','O','C','W','E'},
+				{'L','L','S','H','K','Z','Z','W','Z','C','G','J','U','Y','G'},
+				{'H','S','U','P','J','P','R','J','D','H','S','B','X','T','G'},
+				{'B','R','J','S','O','E','Q','E','T','I','K','K','G','L','E'},
+				{'A','Y','O','A','G','C','I','R','D','Q','H','R','T','C','D'},
+				{'S','C','O','T','T','Y','K','Z','R','E','P','P','X','P','F'},
+				{'B','L','Q','S','L','N','E','E','E','V','U','L','F','M','Z'},
+				{'O','K','R','I','K','A','M','M','R','M','F','B','A','P','P'},
+				{'N','U','I','I','Y','H','Q','M','E','M','Q','R','Y','F','S'},
+				{'E','Y','Z','Y','G','K','Q','J','P','C','Q','W','Y','A','K'},
+				{'S','J','F','Z','M','Q','I','B','D','B','E','M','K','W','D'},
+				{'T','G','L','B','H','C','B','E','C','H','T','O','Y','I','K'},
+				{'O','J','Y','E','U','L','N','C','C','L','Y','B','Z','U','H'},
+				{'W','Z','M','I','S','U','K','U','R','B','I','D','U','X','S'},
+				{'K','Y','L','B','Q','Q','P','M','D','F','C','K','E','A','B'}};
+		assertThat(underTest.buildLetterGrid(starTrekFile), is(starTrekGrid));
+	}
+
 	
 
 }
