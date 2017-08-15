9f70c169b2d259500c7c64bb2ce8a13fedcd62e8
paigeruppel <paige.ruppel@gmail.com>
2017-08-15 15:13:31 -0400

    created second example text file, passed second test in text file reader

--- src/main/java/com/paigeruppel/katas/wordsearch/TextFileReader.java
+++ src/main/java/com/paigeruppel/katas/wordsearch/TextFileReader.java
@@ -1,10 +1,25 @@
 package com.paigeruppel.katas.wordsearch;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TextFileReader {
 
 	public String readFirstLine(String textFile) {
-		// TODO Auto-generated method stub
-		return "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA";
+		String firstLine = "";
+		try {
+			FileReader fileRead = new FileReader(new File(textFile));
+			BufferedReader buffRead = new BufferedReader(fileRead);
+			firstLine = buffRead.readLine();
+			
+			buffRead.close();
+		}
+		catch (IOException e) {
+            e.printStackTrace();
+        }
+		return firstLine;
 	}
 
 }
--- src/test/java/com/paigeruppel/katas/wordsearch/TextFileReaderTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/TextFileReaderTest.java
@@ -15,4 +15,14 @@ public class TextFileReaderTest {
 	
 		assertThat(underTest.readFirstLine(starTrekFile), is("BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA"));
 	}
+	
+	@Test
+	public void shouldReadFirstLineOakSpeciesSearch() {
+		String oakSpeciesFile = "C:\\Users\\Paige\\code\\code-katas\\wordsearch\\src\\main\\resources\\OakSpeciesSearch.txt";
+		TextFileReader underTest = new TextFileReader();
+	
+		assertThat(underTest.readFirstLine(oakSpeciesFile), is("ACERIFOLIA,ALBUS,BICOLOR,COCCINEA,IMBRICARIA,MACROCARPA,PRINUS,RUBRA,SHUMARDII,VELUTINA"));
+	}
+	
+
 }
