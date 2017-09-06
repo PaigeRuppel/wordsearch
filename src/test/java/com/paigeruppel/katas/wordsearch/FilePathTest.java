57f26ada6a084f3d18a597b4f63e8da14d7ba606
paigeruppel <paige.ruppel@gmail.com>
2017-09-06 17:27:11 -0400

    refactoring in FilePathTest

--- src/test/java/com/paigeruppel/katas/wordsearch/FilePathTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/FilePathTest.java
@@ -3,20 +3,25 @@ package com.paigeruppel.katas.wordsearch;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FilePathTest {
 
+	private FilePath underTest;
+	
+	@Before
+	public void setup() {
+		underTest = new FilePath();
+	}
 	
 	@Test
 	public void shouldReturnAbsolutePathForStarTrekSearch() {
-		FilePath underTest = new FilePath();
 		assertThat(underTest.getFullFileName("StarTrekSearch.txt"),is("C:\\Users\\Paige\\code\\code-katas\\wordsearch\\bin\\StarTrekSearch.txt"));
 	}
 	
 	@Test
 	public void shouldReturnAbsolutePathForOakSpeciesSearch() {
-		FilePath underTest = new FilePath();
 		assertThat(underTest.getFullFileName("OakSpeciesSearch.txt"), is("C:\\Users\\Paige\\code\\code-katas\\wordsearch\\bin\\OakSpeciesSearch.txt"));
 	}
 }
