6a9f23025cd95f50f929fed741e9fbd924a973a5
paigeruppel <paige.ruppel@gmail.com>
2017-09-08 11:26:54 -0400

    refactoring in HorizontalFinderTest

--- src/test/java/com/paigeruppel/katas/wordsearch/HorizontalFinderTest.java
+++ src/test/java/com/paigeruppel/katas/wordsearch/HorizontalFinderTest.java
@@ -10,7 +10,8 @@ public class HorizontalFinderTest {
 	private LetterGrid grid;
 	private String toFind;
 
-	public HorizontalFinder createTest(String toFind, LetterGrid grid) {
+	public HorizontalFinder createTest(char[][] toSearch) {
+		grid = new LetterGrid(toSearch);
 		return new HorizontalFinder(toFind, grid);
 	}
 
@@ -18,64 +19,57 @@ public class HorizontalFinderTest {
 	public void shouldReturnCatAtRowOneColumnZeroOneTwo() {
 		toFind = "cat";
 		char[][] toSearch = { { 'x', 'x', 'x' }, { 'c', 'a', 't' }, { 'x', 'x', 'x' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("cat: (0,1),(1,1),(2,1)"));
+		assertThat(createTest(toSearch).scan(), is("cat: (0,1),(1,1),(2,1)"));
 	}
+
 
 	@Test
 	public void shouldReturnCatAtRowZeroColumnZeroOneTwo() {
 		toFind = "cat";
 		char[][] toSearch = { { 'c', 'a', 't' }, { 'x', 'x', 'x' }, { 'x', 'x', 'x' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("cat: (0,0),(1,0),(2,0)"));
+		assertThat(createTest(toSearch).scan(), is("cat: (0,0),(1,0),(2,0)"));
 	}
 
 	@Test
 	public void shouldReturnLaAtRowZeroColumnOneTwo() {
 		toFind = "la";
 		char[][] toSearch = { { 'x', 'l', 'a' }, { 'x', 'x', 'x' }, { 'x', 'x', 'x' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("la: (1,0),(2,0)"));
+		assertThat(createTest(toSearch).scan(), is("la: (1,0),(2,0)"));
 	}
 
 	@Test
 	public void shouldReturnLaAtRowOneColumnOneTwoWithConfoundingL() {
 		toFind = "la";
 		char[][] toSearch = { { 'l', 'x', 'x' }, { 'x', 'l', 'a' }, { 'x', 'x', 'x' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("la: (1,1),(2,1)"));
+		assertThat(createTest(toSearch).scan(), is("la: (1,1),(2,1)"));
 	}
 
 	@Test
 	public void shouldReturnLaAtRowOneColumnOneTwoWithTwoConfoundingL() {
 		toFind = "la";
 		char[][] toSearch = { { 'l', 'x', 'x' }, { 'l', 'l', 'a' }, { 'x', 'x', 'x' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("la: (1,1),(2,1)"));
+		assertThat(createTest(toSearch).scan(), is("la: (1,1),(2,1)"));
 	}
 
 	@Test
 	public void shouldReturnLabAtRowTwoColumnZeroOneTwoWithConfoundingLa() {
 		toFind = "lab";
 		char[][] toSearch = { { 'l', 'x', 'x' }, { 'l', 'l', 'a' }, { 'l', 'a', 'b' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("lab: (0,2),(1,2),(2,2)"));
+		assertThat(createTest(toSearch).scan(), is("lab: (0,2),(1,2),(2,2)"));
 	}
 
 	@Test
 	public void shouldReturnLabAtRowTwoColumnTwoOneZeroWithConfoundingLa() {
 		toFind = "lab";
 		char[][] toSearch = { { 'l', 'x', 'x' }, { 'x', 'x', 'x' }, { 'b', 'a', 'l' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("lab: (2,2),(1,2),(0,2)"));
+		assertThat(createTest(toSearch).scan(), is("lab: (2,2),(1,2),(0,2)"));
 	}
 
 	@Test
 	public void shouldReturnNotFoundForCat() {
 		toFind = "cat";
 		char[][] toSearch = { { 'l', 'x', 'x' }, { 'x', 'x', 'x' }, { 'b', 'a', 'l' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("not found"));
+		assertThat(createTest(toSearch).scan(), is("not found"));
 	}
 
 	@Test
@@ -83,8 +77,7 @@ public class HorizontalFinderTest {
 		toFind = "bill";
 		char[][] toSearch = { { 'b', 'i', 'l', 'l' }, { 'x', 'x', 'x', 'x' }, { 'x', 'x', 'x', 'x' },
 				{ 'x', 'b', 'a', 'l' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("bill: (0,0),(1,0),(2,0),(3,0)"));
+		assertThat(createTest(toSearch).scan(), is("bill: (0,0),(1,0),(2,0),(3,0)"));
 	}
 
 	@Test
@@ -105,7 +98,6 @@ public class HorizontalFinderTest {
 				{ 'O', 'J', 'Y', 'E', 'U', 'L', 'N', 'C', 'C', 'L', 'Y', 'B', 'Z', 'U', 'H' },
 				{ 'W', 'Z', 'M', 'I', 'S', 'U', 'K', 'U', 'R', 'B', 'I', 'D', 'U', 'X', 'S' },
 				{ 'K', 'Y', 'L', 'B', 'Q', 'Q', 'P', 'M', 'D', 'F', 'C', 'K', 'E', 'A', 'B' } };
-		grid = new LetterGrid(toSearch);
-		assertThat(createTest(toFind, grid).scan(), is("KIRK: (4,7),(3,7),(2,7),(1,7)"));
+		assertThat(createTest(toSearch).scan(), is("KIRK: (4,7),(3,7),(2,7),(1,7)"));
 	}
 }
