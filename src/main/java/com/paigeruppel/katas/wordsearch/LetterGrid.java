d1b94bd9b539577c29820d020af5c575db8c29a5
paigeruppel <paige.ruppel@gmail.com>
2017-09-08 13:30:07 -0400

    removed all methods and tests for checking for a next char from current position - going with a different approcah in the if/else blocks in each of the finders

--- src/main/java/com/paigeruppel/katas/wordsearch/LetterGrid.java
+++ src/main/java/com/paigeruppel/katas/wordsearch/LetterGrid.java
@@ -29,44 +29,4 @@ public class LetterGrid {
 		return toSearch[y][x];
 	}
 
-//	public Character getNextCharacterHorizontalFrom(int y, int x) {
-//		Character nextHorChar;
-//		if (x + 1 < toSearch[y].length) {
-//			nextHorChar = toSearch[y][x + 1];
-//		} else {
-//			nextHorChar = '0';
-//		}
-//		return nextHorChar;
-//	}
-//
-//	public Character getNextCharacterVerticalFrom(int x, int y) {
-//		Character nextVerChar;
-//		if (y + 1 < toSearch.length) {
-//			nextVerChar = toSearch[y + 1][x];
-//		} else {
-//			nextVerChar = '0';
-//		}
-//		return nextVerChar;
-//	}
-//
-//	public Character getNextCharacterLToRDiagonalFrom(int y, int x) {
-//		Character nextLToRDiagChar;
-//		if (x + 1 < toSearch[0].length && y + 1 < toSearch.length) {
-//			nextLToRDiagChar = toSearch[y + 1][x + 1];
-//		} else {
-//			nextLToRDiagChar = '0';
-//		}
-//		return nextLToRDiagChar;
-//	}
-//
-//	public Character getNextCharacterRToLDiagonalFrom(int y, int x) {
-//		Character nextRToLDiagChar;
-//		if (x - 1 > -1 && y + 1 < toSearch.length) {
-//			nextRToLDiagChar = toSearch[y + 1][x - 1];
-//		} else {
-//			nextRToLDiagChar = '0';
-//		}
-//		return nextRToLDiagChar;
-//	}
-
 }
